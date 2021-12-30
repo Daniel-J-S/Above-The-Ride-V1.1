e179c92f5f2d68cc3e74a00dac350866df0f7f47
Daniel JS <daniel@mydeveloperjourney.com>
2021-12-30 16:50:01 -0600

    wip: adds initial setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,5 +50,20 @@ module.exports = {
         language: null
       },
     },
+    // {
+    //   resolve: `gatsby-plugin-gdpr-cookies`,
+    //   options: {
+    //     googleAnalytics: {
+    //       trackingId: 'YOUR_GOOGLE_ANALYTICS_TRACKING_ID',
+    //       // Setting this parameter is optional
+    //       anonymize: true
+    //     },
+    //     facebookPixel: {
+    //       pixelId: 'YOUR_FACEBOOK_PIXEL_ID'
+    //     },
+    //     // Defines the environments where the tracking should be available  - default is ["production"]
+    //     environments: ['production', 'development']
+    //   },
+    // },
   ],
 }
