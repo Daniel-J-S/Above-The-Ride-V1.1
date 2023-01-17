1fdfa5945a9bf01b699f654030dda49569b38b7e
myDeveloperJourney <daniel@mydeveloperjourney.com>
2023-01-16 18:29:09 -0600

    wip: adds update to logo styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,18 @@
-import { Link } from 'gatsby';
-import React, { useEffect, useState } from 'react';
-import whiteLogo from '../images/white-logo.svg';  
-import blackLogo from '../images/black-logo.svg';  
+import {
+  Link
+} from 'gatsby';
+import React, {
+  useEffect,
+  useState
+} from 'react';
+import whiteLogo from '../images/white-logo.svg';
+import blackLogo from '../images/black-logo.svg';
 import Burger from './burger';
 import NavDrawer from './navDrawer';
 import useScrollFromTopDetected from '../hooks/useScrollFromTopDetected';
-import { useLocation } from '@reach/router';
+import {
+  useLocation
+} from '@reach/router';
 
 const Header = ({
     navOpen,
@@ -16,63 +23,104 @@ const Header = ({
     showItemsCount,
   }) => {
 
-  const location = useLocation();
-  const isPastTop = useScrollFromTopDetected();
-  const [cartOpen, setCartOpen] = useState(false);
+    const location = useLocation();
+    const isPastTop = useScrollFromTopDetected();
+    const [cartOpen, setCartOpen] = useState(false);
 
-  const handleClick = () => {
-    if(cartOpen && typeof window !== 'undefined') {
-      window.Snipcart.api.theme.cart.close()
+    const handleClick = () => {
+      if (cartOpen && typeof window !== 'undefined') {
+        window.Snipcart.api.theme.cart.close()
+      }
     }
-  }
-  
-  const styles = { 
+
+    const styles = {
       headerStyles: {
-        backgroundColor: isPastTop ? '#fff': navOpen ? '#000000' : 'transparent', 
-        borderBottom: isPastTop ? '1px solid #808080': 'none',
-        boxShadow: isPastTop ? '1px 1px 10px 1px #333': 'none',
+        backgroundColor: isPastTop ? '#fff' : navOpen ? '#000000' : 'transparent',
+        borderBottom: isPastTop ? '1px solid #808080' : 'none',
+        boxShadow: isPastTop ? '1px 1px 10px 1px #333' : 'none',
       },
       linkStyles: {
-        cart: {cursor: 'pointer', color: isPastTop ? '#000': '#fff'},
-        link: { color: isPastTop ? '#000': '#fff'}
+        cart: {
+          cursor: 'pointer',
+          color: isPastTop ? '#000' : '#fff'
+        },
+        link: {
+          color: isPastTop ? '#000' : '#fff'
+        }
+      },
+      logo: {
+        whiteLogo: {
+          display: 'block'
+        },
+        blackLogo: {
+          display: 'block'
+        },
       }
-  };
+    };
 
-  if(navOpen) {
-    Object.assign(styles.headerStyles, {
-      boxShadow: 'none',
-    });
-  }
+    if (!isPastTop) {
+      Object.assign(styles.logo.blackLogo, {
+        display: 'none'
+      });
+    } else if (!cartOpen) {
+      Object.assign(styles.logo.whiteLogo, {
+        display: 'none'
+      });
+    }
 
-  if(cartOpen) {
-    Object.assign(styles.headerStyles, {
-      backgroundColor: '#000000', 
-      borderBottom: 'none',
-      boxShadow: '1px 1px 10px 1px #333',
-    });
-    Object.assign(styles.linkStyles, {
-      cart: {cursor: 'pointer', color: '#fff'},
-      link: { color: '#fff'}
-    });
-  }
+    if (navOpen) {
+      Object.assign(styles.headerStyles, {
+        boxShadow: 'none',
+      });
+      Object.assign(styles.logo.blackLogo, {
+        display: 'none'
+      });
+    }
 
-  if(location.pathname !== '/') {
-    Object.assign(styles.headerStyles, {
-      backgroundColor: '#000000', 
-      borderBottom: 'none',
-    });
-    Object.assign(styles.linkStyles, {
-      cart: {color: '#fff'},
-      link: { color: '#fff'}
-    });
-  }
+    if (cartOpen) {
+      Object.assign(styles.headerStyles, {
+        backgroundColor: '#000000',
+        borderBottom: 'none',
+        boxShadow: '1px 1px 10px 1px #333',
+      });
+      Object.assign(styles.linkStyles, {
+        cart: {
+          cursor: 'pointer',
+          color: '#fff'
+        },
+        link: {
+          color: '#fff'
+        }
+      });
+      Object.assign(styles.logo.blackLogo, {
+        display: 'none'
+      });
+    }
 
-  useEffect(() => {
-    const condition = ['#/cart'].includes(location.hash);
-    setCartOpen(condition);
-  }, [location.hash]);
+    if (location.pathname !== '/') {
+      Object.assign(styles.headerStyles, {
+        backgroundColor: '#000000',
+        borderBottom: 'none',
+      });
+      Object.assign(styles.linkStyles, {
+        cart: {
+          color: '#fff'
+        },
+        link: {
+          color: '#fff'
+        }
+      });
+      Object.assign(styles.logo.blackLogo, {
+        display: 'none'
+      });
+    }
 
-  return (
+    useEffect(() => {
+      const condition = ['#/cart'].includes(location.hash);
+      setCartOpen(condition);
+    }, [location.hash]);
+
+    return (
     <>
     <header
       className="site-header" 
@@ -99,13 +147,8 @@ const Header = ({
       </section>
       <section>
         <Link onClick={() => setNavOpen(false)} className="header-logo" to="/">
-          { (!isPastTop || navOpen || cartOpen) ?
-            <img src={whiteLogo} alt="Above the ride"></img>
-            : location.pathname !== '/' ?
-            <img src={whiteLogo} alt="Above the ride"></img>
-            :
-            <img src={blackLogo} alt="Above the ride"></img>
-          }
+          <img style={styles.logo.whiteLogo} src={whiteLogo} alt="Above the ride"></img>
+          <img style={styles.logo.blackLogo} src={blackLogo} alt="Above the ride"></img>
         </Link>
       </section>
       <section>
